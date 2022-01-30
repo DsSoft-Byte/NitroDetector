fa922ea0e271565467579cfa90a04c717d105c5e
DsSoft-Byte <69913272+DsSoft-Byte@users.noreply.github.com>
2022-01-30 18:36:46 +0100

    Delete NitroDetector - Version- 1.0 - (source Code)/NitroDetector directory

--- NitroDetector - Version- 1.0 - (source Code)/NitroDetector/Form3.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form3
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        IO.File.ReadAllText("C:\LEGIT.txt")
-        TextBox1.Text = ("Didnt Error Out? Everything is OK. Error? Files Are Missing")
-    End Sub
-End Class
